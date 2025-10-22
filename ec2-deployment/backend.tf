e14ab84cdafbb4ec7c75e7872e76ec2f575816d9
sudhakar9666 <149573730+sudhakar9666@users.noreply.github.com>
2025-10-22 12:33:29 +0530

    Update backend.tf

--- ec2-deployment/backend.tf
+++ ec2-deployment/backend.tf
@@ -3,7 +3,5 @@ terraform {
     bucket         = "my-terraform-state-565871519906" # EDIT
     key            = "infra/terraform.tfstate"                 # EDIT if you want
     region         = "us-east-1"                               # EDIT
-    dynamodb_table = "tf-locks"                               # EDIT
-    encrypt        = true
   }
 }
