1d7f12716b04520a68dda45ac6d121825d11891a
sudhakar9666 <149573730+sudhakar9666@users.noreply.github.com>
2025-10-22 13:01:57 +0530

    Update outputs.tf

--- ec2-deployment/outputs.tf
+++ ec2-deployment/outputs.tf
@@ -6,11 +6,11 @@ output "public_subnet_id" {
   value = aws_subnet.my-subnet.id
 }
 
-output "ec2_public_ip" {
+/*output "ec2_public_ip" {
   value = aws_instance.web.public_ip
 }
 
-/*output "ec2_ssh_command" {
+output "ec2_ssh_command" {
   value = "ssh -i ~/.ssh/${var.key_name}.pem ubuntu@${aws_instance.web.public_ip}"
 }
 
